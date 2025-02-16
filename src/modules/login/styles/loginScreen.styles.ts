59aede6c73f5f46b955be01d9453e05551ba5e6b
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2025-02-16 16:21:08 -0300

    :sparkles: feat: change layout and create button component

--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { Space, Typography } from 'antd';
+
+const {Title,  Link} = Typography;
 
 
 export const ContainerLoginScreen = styled.div`
@@ -19,7 +22,13 @@ export const BackgroundImage = styled.img`
 `;
 
 export const LogoImage = styled.img`
+    width: 202px;
 `;
+
+export const TitleLogin = styled(Title)`
+    color: #006397 ;
+`;
+
 
 export const LoginContainer = styled.div`
     display: flex;
@@ -39,5 +48,6 @@ export const LimitedContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center; 
+    align-items: center;
     flex-direction: column;
 `;
